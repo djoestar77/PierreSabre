1d3d9fb2e37ffa618f24ef68e3de6eca1e40e75b
djoestar77 <144283375+djoestar77@users.noreply.github.com>
2026-05-05 17:37:04 +0200

    TP5 init

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -4,6 +4,8 @@ public class Humain {
 	private String nom;
 	private String boissonFav;
 	private int wari;
+	protected int nbConnaissances;
+	protected Humain[] memoire = new Humain[30];
 	
 	
 	
@@ -42,6 +44,51 @@ public class Humain {
 		} else {
 			this.parler("Je n'ai plus que "+this.wari+" sous en poche je ne peux meme pas m'offrir "+bien+" à "+prix+" sous");
 			return false;
+		}
+	}
+
+	public int faireConnaissanceAvec(Humain autreHumain) {
+		this.direBonjour();
+		autreHumain.repondre(this);
+
+		if (this.nbConnaissances < 30) {
+			this.memoriser(autreHumain);
+			this.parler("Enchanté "+autreHumain.getNom()+" !");
+			return this.nbConnaissances;
+		} else {
+			this.parler("Je ne peux plus me souvenir de "+autreHumain.getNom()+" ...");
+			return this.nbConnaissances;
+		}
+	}
+
+	private void memoriser(Humain autreHumain) {
+		if (this.nbConnaissances < 30) {
+			this.memoire[this.nbConnaissances] = autreHumain;
+			this.nbConnaissances++;
+		} else {
+			for (int i = 0; i < this.nbConnaissances-1; i++) {
+				this.memoire[i] = this.memoire[i + 1];
+			}
+			this.memoire[this.nbConnaissances-1] = autreHumain;
+		}
+	}
+
+	private void repondre(Humain autreHumain) {
+		this.direBonjour();
+	}
+
+	public void listerConnaissances() {
+		if (this.nbConnaissances == 0) {
+			this.parler("Je ne connais personne ...");
+		} else {
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < this.nbConnaissances; i++) {
+				sb.append(this.memoire[i].getNom());
+				if (i < this.nbConnaissances - 1) {
+					sb.append(", ");
+				}
+			}
+			this.parler("Je connais les personnes suivantes : " + sb.toString());
 		}
 	}
 	
